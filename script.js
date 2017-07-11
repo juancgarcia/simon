a8eefa60bed858320f8c2123373cdc04005ea181
Alex Wandres <alex.r.wandres@gmail.com>
2017-07-11 15:08:26 -0400

    enter key initiates game, added reactive HTML elements

--- script.js
+++ script.js
@@ -10,38 +10,65 @@ var colorSequence = ['#green', '#blue', '#red', '#yellow' ]
 var userSequence = []
 var level = 4
 var turn = 0
+var counter = 0
+var gameReady = true
+var levelCount = 1
+var userReady = true
 
+
+//Randomizes game sequence, the length of which is determined by level
 function randomColors() {
-
   for (i = 0; i < level ; i++){
     var number = Math.floor(Math.random() * 4)
     gameSequence.push(colorSequence[number])
   }
 }
 
-// function nextLevel() {
-//   var addColor = Math.floor(Math.random() * 4)
-//   gameSequence.push(colorSequence[addColor])
-//   counter = 0
-//   for (i = 0; i < gameSequence.length; i++) {
-//     var number = Math.floor(Math.random() * gameSequence.length)
-//     var thisColor = gameSequence[number]
-//     gameSequence.push(thisColor)
-//     gameSequence.shift()
-//   }
-// }
+//Upon each keypress, evaluates if the key was correct according to the turn(order of array)
+function evaluate() {
+  if (userSequence[turn] === gameSequence[turn]) {
+    turn++
+    if (userSequence.length === gameSequence.length) {
+      endLevel()
+    }
+  }
+  else {
+    $('#prompt').text('Wrong Input. Press Enter to play again')
+    userSequence = []
+    gameSequence = []
+    turn = 0
+    counter = 0
+    level = 4
+    levelCount = 1
+    gameReady = true
+    randomColors()
+  }
+}
 
+//Upon completion, resets to original conditions but adds a level
+function endLevel() {
+  if (JSON.stringify(userSequence) === JSON.stringify(gameSequence)) {
+    console.log('You Win')
+    userSequence = []
+    gameSequence = []
+    counter = 0
+    turn = 0
+    gameReady = true
+    level ++
+    levelCount ++
+    randomColors()
+    $('#prompt').text('Level Complete! Press Enter to move on')
+  }
+}
 
-randomColors()
-var counter = 0
-
+//lights up colors blocks according to random collor array
 function startGame() {
+    $('#prompt').text('')
+    $('#level').text(`Level : ${levelCount}`)
     var thisSquare = gameSequence[counter]
-    var lastSquare = gameSequence[counter-1]
     console.log(gameSequence[counter])
-    // $(lastSquare).css('opacity', '.5')
     setTimeout(function(){
-    $(thisSquare).css('opacity', '1')}, 200)
+      $(thisSquare).css('opacity', '1')}, 200)
     setTimeout(function(){
       $(thisSquare).css('opacity', '.5')}, 1000)
     counter++
@@ -52,52 +79,28 @@ function startGame() {
 }
 
 
+randomColors()
+
+
+
 $('html').keydown(function(e) {
   if(e.which === 38) {
     userSequence.push('#red')
     red.css('opacity', '1')
     setTimeout(function() {
       red.css('opacity', '.5')}, 200)
-    if (userSequence[turn] === gameSequence[turn]) {
-      turn++
-      if (userSequence.length === gameSequence.length) {
-        endLevel()
-      }
-    }
-    else {
-      console.log('Wrong')
-      userSequence = []
-      gameSequence = []
-      turn = 0
-      counter = 0
-      level = 4
-      randomColors()
-    }
+      evaluate()
   }
 })
 
-
+// ARROW BUTTON ACTIONS
 $('html').keydown(function(e) {
   if(e.which === 37) {
     userSequence.push('#blue')
     blue.css('opacity', '1')
     setTimeout(function() {
       blue.css('opacity', '.5')}, 200)
-      if (userSequence[turn] === gameSequence[turn]) {
-        turn++
-        if (userSequence.length === gameSequence.length) {
-          endLevel()
-        }
-      }
-      else {
-        console.log('Wrong')
-        userSequence = []
-        gameSequence = []
-        turn = 0
-        counter = 0
-        level = 4
-        randomColors()
-      }
+      evaluate()
   }
 })
 
@@ -107,21 +110,7 @@ $('html').keydown(function(e) {
     green.css('opacity', '1')
     setTimeout(function() {
       green.css('opacity', '.5')}, 200)
-      if (userSequence[turn] === gameSequence[turn]) {
-        turn++
-        if (userSequence.length === gameSequence.length) {
-          endLevel()
-        }
-      }
-      else {
-        console.log('Wrong')
-        userSequence = []
-        gameSequence = []
-        turn = 0
-        counter = 0
-        level = 4
-        randomColors()
-      }
+      evaluate()
   }
 })
 
@@ -131,37 +120,14 @@ $('html').keydown(function(e) {
     yellow.css('opacity', '1')
     setTimeout(function() {
       yellow.css('opacity', '.5')}, 200)
-      if (userSequence[turn] === gameSequence[turn]) {
-        turn++
-        if (userSequence.length === gameSequence.length) {
-          endLevel()
-        }
-      }
-      else {
-        console.log('Wrong')
-        userSequence = []
-        gameSequence = []
-        turn = 0
-        counter = 0
-        level = 4
-        randomColors()
-      }
+      evaluate()
 
   }
 })
 
-
-
-endLevel()
-
-function endLevel() {
-  if (JSON.stringify(userSequence) === JSON.stringify(gameSequence)) {
-    console.log('You Win')
-    userSequence = []
-    gameSequence = []
-    counter = 0
-    turn = 0
-    level ++
-    randomColors()
+$('html').keydown(function(e) {
+  if (e.which === 13) {
+    gameReady = false
+    startGame()
   }
-}
+})
